05f0412530f5e40446508cb659970d9e6bc9acdb
Shauin Mac <shinjitsukasa09@gmail.com>
2024-02-07 23:30:45 +0800

    setup prisma connection to planetscale

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useState } from "react";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -16,6 +17,8 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+	const [loading, setLoading] = useState();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -45,17 +48,17 @@ export const StoreModal = () => {
                 <FormItem>
                   <FormLabel>Name:</FormLabel>
                   <FormControl>
-                    <Input placeholder="E-Commerce" {...field} />
+                    <Input disabled={loading} placeholder="E-Commerce" {...field} />
                   </FormControl>
 									<FormMessage />
                 </FormItem>
               )}
             />
             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-              <Button variant="outline" onClick={storeModal.onClose}>
+              <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>
                 Cancel
               </Button>
-              <Button>Continue</Button>
+              <Button disabled={loading} type="submit">Continue</Button>
             </div>
           </form>
         </Form>
