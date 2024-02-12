aa501a0b613c61ee2771677234d49d9c65e56da5
Shauin Mac <shinjitsukasa09@gmail.com>
2024-02-12 14:39:05 +0800

    billboard updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: [
+			"avatars.githubusercontent.com",
+			"lh3.googleusercontent.com",
+			"res.cloudinary.com"
+		]
+	}
+};
 
 export default nextConfig;
+
+// module.exports = {
+// 	images: {
+// 		remotePatterns: [
+// 			{
+// 				protocols: 'https',
+// 				hostname: 'res.cloudinary.com',
+// 				hostname: 'lh3.googleusercontent.com',
+// 				hostname: 'avatars.githubusercontent.com',
+// 				port: '',
+// 				pathname: '**'
+// 			}
+// 		]
+// 	}
+// }
+
+// module.exports = nextConfig;
